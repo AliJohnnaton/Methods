2b12d4db75ce0c3022b08c08d0672fbf187c952f
AliJohnnaton <trofimov2002@mail.ru>
2025-03-17 12:57:57 +0300

    Refactor

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
@@ -7,56 +9,59 @@ public class Main {
         System.out.println("\nЗадание 2");
         int clientOS = 1;
         int clientDeviceYear = 2014;
-        secondTask(clientOS, clientDeviceYear);
+        checkOS(clientOS, clientDeviceYear);
 
         System.out.println("\nЗадание 3");
         int deliveryDistance = 95;
-        thirdTask(deliveryDistance);
+        int days = calculateDays(deliveryDistance);
+        if (days > 0) {
+            System.out.print("Потребуеться дней на доставку: " + days);
+        } else {
+            System.out.println("Доставка на дакую дистанцию не возможна");
+        }
     }
 
     public static void checkYear(int year) {
         System.out.print(year + " год - ");
-        if (year % 100 == 0 && year % 400 != 0)
+        if (year % 100 == 0 && year % 400 != 0) {
             System.out.print("не високосный");
-        else if (year % 400 == 0)
+        } else if (year % 400 == 0) {
             System.out.println("високосный");
-        else if (year % 4 == 0)
+        } else if (year % 4 == 0) {
             System.out.println("високосный");
-        else System.out.println("не високосный");
-    }
-
-    public static void secondTask(int clientOS, int clientDeviceYear) {
-        if (clientDeviceYear >= 2015) {
-            if (clientOS == 1)
-                System.out.println("Установите версию приложения для Android по ссылке: " +
-                        "https//:skachat-samoe-krutoe-prilozhenie-banka.ru/download-app/android/");
-            else if (clientOS == 0)
-                System.out.println("Установите версию приложения для iOS по ссылке: " +
-                        "https//:skachat-samoe-krutoe-prilozhenie-banka.ru/download-app/ios/");
-            else
-                System.out.println("У вас там что Linux что-ли? Или Windows?");
         } else {
-            if (clientOS == 1)
-                System.out.println("Установите облегчённую версию приложения для Android по ссылке: " +
-                        "https//:skachat-prilozhenie-banka.ru/download-app-lite/android/");
-            else if (clientOS == 0)
-                System.out.println("Установите облегчённую версию приложения для iOS по ссылке: " +
-                        "https//:skachat-prilozhenie-banka.ru/download-app-lite/ios/");
-            else
-                System.out.println("У вас там что BlackBerry что-ли? Или Nokia?");
+            System.out.println("не високосный");
         }
     }
 
-    public static void thirdTask(int deliveryDistance) {
+    public static void checkOS(int clientOS, int clientDeviceYear) {
+
+        if (clientOS == 1 && clientDeviceYear >= LocalDate.now().getYear()) {
+            System.out.println("Установите версию приложения для Android по ссылке: " +
+                    "https//:skachat-samoe-krutoe-prilozhenie-banka.ru/download-app/android/");
+        } else if (clientOS == 0 && clientDeviceYear >= LocalDate.now().getYear()) {
+            System.out.println("Установите версию приложения для iOS по ссылке: " +
+                    "https//:skachat-samoe-krutoe-prilozhenie-banka.ru/download-app/ios/");
+        } else if (clientOS == 1) {
+            System.out.println("Установите облегчённую версию приложения для Android по ссылке: " +
+                    "https//:skachat-prilozhenie-banka.ru/download-app-lite/android/");
+        } else if (clientOS == 0) {
+            System.out.println("Установите облегчённую версию приложения для iOS по ссылке: " +
+                    "https//:skachat-prilozhenie-banka.ru/download-app-lite/ios/");
+        }
+    }
+
+    public static int calculateDays(int deliveryDistance) {
         System.out.print("Потребуеться дней: ");
-        if (deliveryDistance < 20)
-            System.out.print(1);
-        else if (deliveryDistance < 60)
-            System.out.print(2);
-        else if (deliveryDistance <= 100)
-            System.out.print(3);
-        else
-            System.out.print("Error, доставка на такую дистанцию не возможна");
+        if (deliveryDistance < 20) {
+            return 1;
+        } else if (deliveryDistance < 60) {
+            return 2;
+        } else if (deliveryDistance <= 100) {
+            return 3;
+        } else {
+            return -1;
+        }
     }
 
 }
