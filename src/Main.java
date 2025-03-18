bc8f7bba21a20639a0b675ebbd71b8319f5edb13
AliJohnnaton <trofimov2002@mail.ru>
2025-03-18 20:35:51 +0300

    Refactor2

--- src/Main.java
+++ src/Main.java
@@ -23,36 +23,31 @@ public class Main {
 
     public static void checkYear(int year) {
         System.out.print(year + " год - ");
-        if (year % 100 == 0 && year % 400 != 0) {
-            System.out.print("не високосный");
-        } else if (year % 400 == 0) {
-            System.out.println("високосный");
-        } else if (year % 4 == 0) {
-            System.out.println("високосный");
+        if ((year % 100 != 0 && year % 4 == 0) || year % 400 == 0) {
+            System.out.print("високосный");
         } else {
             System.out.println("не високосный");
         }
     }
 
     public static void checkOS(int clientOS, int clientDeviceYear) {
-
-        if (clientOS == 1 && clientDeviceYear >= LocalDate.now().getYear()) {
+        int dateNow = LocalDate.now().getYear();
+        if (clientOS == 1 && clientDeviceYear >= dateNow) {
             System.out.println("Установите версию приложения для Android по ссылке: " +
                     "https//:skachat-samoe-krutoe-prilozhenie-banka.ru/download-app/android/");
-        } else if (clientOS == 0 && clientDeviceYear >= LocalDate.now().getYear()) {
+        } else if (clientOS == 0 && clientDeviceYear >= dateNow) {
             System.out.println("Установите версию приложения для iOS по ссылке: " +
                     "https//:skachat-samoe-krutoe-prilozhenie-banka.ru/download-app/ios/");
-        } else if (clientOS == 1) {
+        } else if (clientOS == 1 && clientDeviceYear < dateNow) {
             System.out.println("Установите облегчённую версию приложения для Android по ссылке: " +
                     "https//:skachat-prilozhenie-banka.ru/download-app-lite/android/");
-        } else if (clientOS == 0) {
+        } else {
             System.out.println("Установите облегчённую версию приложения для iOS по ссылке: " +
                     "https//:skachat-prilozhenie-banka.ru/download-app-lite/ios/");
         }
     }
 
     public static int calculateDays(int deliveryDistance) {
-        System.out.print("Потребуеться дней: ");
         if (deliveryDistance < 20) {
             return 1;
         } else if (deliveryDistance < 60) {
